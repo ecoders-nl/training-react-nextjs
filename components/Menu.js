aa3f7535fb45a30908146e72e6fe4aab4d6fc6b6
Tom Greuter <info@tomgreuter.nl>
2022-01-25 10:13:11 +0100

    Add some styling to menu

--- components/Menu.js
+++ components/Menu.js
@@ -1,18 +1,19 @@
 import Link from "next/link";
+import styles from "../styles/Menu.module.css";
 
 export default function Menu() {
   return (
-    <ul>
-      <li>
+    <ul className={styles.list}>
+      <li className={styles.listitem}>
         <Link href="/">Homepage</Link>
       </li>
-      <li>
+      <li className={styles.listitem}>
         <Link href="/page2">Page 2</Link>
       </li>
-      <li>
+      <li className={styles.listitem}>
         <Link href="/about">About</Link>
       </li>
-      <li>
+      <li className={styles.listitem}>
         <Link href="/page4">Page 4</Link>
       </li>
     </ul>
